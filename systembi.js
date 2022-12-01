ee80fe8bda6644186104721f243df0dfec3f7e0b
Oscar Rodrgues <oscar.rodrigues.dev@gmail.com>
2022-12-01 17:01:06 -0300

    design na pagina da internet sofreu pequenas alterações, foram implementados algumas funcionalidades basicas de programação javascript

--- systembi.js
+++ systembi.js
@@ -1,10 +1,10 @@
 //arquivo javascript
-//alert('Bem vindo ao SBI!')
+let matricula = ""
 
 /*essa função serve para limpar o campo de texto na pagina de controle de estoque*/
 function clear_all(decisão) {
-	//alert('botão pressionado!' + decisão)
-	var select = document.getElementById("eqpms");
+
+ var select = document.getElementById("eqpms");
  var equipamento = select.options[select.selectedIndex].text;
 
  var text = document.getElementById("serial");
@@ -13,10 +13,15 @@ function clear_all(decisão) {
  var data = document.getElementById("data");
     var StringData =data.value; 
   if( decisão=="entrada"){
-	alert(equipamento +  " foi adcionado com sucesso!")
+  	 matricula=  prompt("Para finalizar digite sua matricula:")
+	alert(equipamento +  " foi adcionado com sucesso por "+ matricula)
+
 }else{
-  alert(equipamento + " foi retirado do estoque")
-}
+	 matricula=  prompt("Para finalizar digite sua matricula:")
+
+
+  alert(equipamento +  " foi retirado com sucesso por "+ matricula)
+ }
 	
 	select.selectedIndex = 0//define o painel inicial
 	text.value = ""
@@ -25,11 +30,38 @@ function clear_all(decisão) {
 	var dia =  hoje.getDate()
     var mes =  hoje.getMonth()+1
     var ano =  hoje.getFullYear()
-	
-	alert(dia)
-	alert(mes)
-	alert(ano)
 	data.value = "dd/mm/aaaa"
 	
 
 }
+//preciso criar uma função que analise se os campos informados estã devidamente preenchidos
+
+function ver_event() {
+alert('escutando eventos')
+}
+
+/*função para acesso ao estoque armazenado*/
+function acessar_estoque(classe) {
+   
+   estoque = document.getElementById(classe).style;//estoque
+   controle =document.querySelector('.formulario1').style;
+   estoque.display = 'block'
+   controle.display= 'none'
+  
+
+}
+
+   function acessar_controle(classe) {
+
+   controle =document.querySelector(classe).style;
+   estoque = document.getElementById('estoque').style;//estoque
+   controle.display='block'
+   estoque.display='none'
+
+   	
+   	
+  
+   }
+
+
+ 
